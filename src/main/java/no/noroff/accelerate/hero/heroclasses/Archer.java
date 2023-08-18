427c440e4397388253dfa3c0d87a3cbeabeb24c0
MichalPajestka <001michal002@gmail.com>
2023-08-18 12:15:44 +0200

    Rewrtoe the archer tests because they were not meeting some requirements

--- src/main/java/no/noroff/accelerate/hero/heroclasses/Archer.java
+++ src/main/java/no/noroff/accelerate/hero/heroclasses/Archer.java
@@ -1,67 +1,68 @@
-package no.noroff.accelerate.hero.heroclasses;
+    package no.noroff.accelerate.hero.heroclasses;
 
-import no.noroff.accelerate.exceptions.weapons.ArcherInvalidWeaponException;
-import no.noroff.accelerate.hero.Hero;
-import no.noroff.accelerate.hero.HeroAttribute;
-import no.noroff.accelerate.hero.HeroClass;
-import no.noroff.accelerate.items.weapons.Weapon;
-import no.noroff.accelerate.items.weapons.WeaponType;
+    import no.noroff.accelerate.exceptions.weapons.ArcherInvalidWeaponException;
+    import no.noroff.accelerate.hero.Hero;
+    import no.noroff.accelerate.hero.HeroAttribute;
+    import no.noroff.accelerate.hero.HeroClass;
+    import no.noroff.accelerate.items.weapons.Weapon;
+    import no.noroff.accelerate.items.weapons.WeaponType;
 
-import java.util.Arrays;
+    import java.util.Arrays;
 
-public class Archer extends Hero {
-    public Archer(String name) {
-        super(name, HeroClass.ARCHER);
-        this.levelAttributes = new HeroAttribute(1, 7, 1);
-        this.validWeaponTypes = Arrays.asList(WeaponType.BOW);
-    }
-
-    @Override
-    public HeroAttribute getAttributeLevelUp() {
-        return new HeroAttribute(1, 5, 1);
-    }
-
-    @Override
-    protected void levelUpAttributes() {
-        levelAttributes.addAttributes(new HeroAttribute(1, 5, 1));
-    }
-
-    @Override
-    public double calcDamage() {
-        Weapon equippedWeapon = getEquippedWeapon();
-
-        if (equippedWeapon != null) {
-            int weaponDamage = equippedWeapon.getWeaponDamage();
-            double damageAttribute = calcTotalAttributes().getDexterity() / 100.0;
-            return weaponDamage * (1 + damageAttribute);
-        } else {
-            return 1.0;
-        }
-    }
-
-    @Override
-    public void equipWeapon(Weapon weapon) throws ArcherInvalidWeaponException {
-        if (level < weapon.getRequiredLevel()) {
-            throw new ArcherInvalidWeaponException("Hero level is too low to equip this weapon");
+    public class Archer extends Hero {
+        public Archer(String name) {
+            super(name, HeroClass.ARCHER);
+            this.levelAttributes = new HeroAttribute(1, 7, 1);
+            this.validWeaponTypes = Arrays.asList(WeaponType.BOW);
         }
 
-        if (!WeaponType.BOW.equals(weapon.getWeaponType())) {
-            throw new ArcherInvalidWeaponException("Archers can only use bows");
+        @Override
+        public HeroAttribute getAttributeLevelUp() {
+            return new HeroAttribute(1, 5, 1);
         }
 
-        equipment.equipItem(weapon);
-    }
+        @Override
+        protected void levelUpAttributes() {
+            levelAttributes.addAttributes(new HeroAttribute(1, 5, 1));
+        }
 
-    public String getName() {
-        return name;
-    }
+        @Override
+        public double calcDamage() {
+            Weapon equippedWeapon = getEquippedWeapon();
 
-    public int getLevel() {
-        return level;
-    }
+            if (equippedWeapon != null) {
+                int weaponDamage = equippedWeapon.getWeaponDamage();
+                double damageAttribute = calcTotalAttributes().getDexterity() / 100.0;
+                double damage = weaponDamage * (1 + damageAttribute);
+                return (double) Math.round(damage * 100.0) / 100;
+            } else {
+                return 1;
+            }
+        }
 
-    public HeroAttribute getLevelAttributes() {
-        return levelAttributes;
-    }
+        @Override
+        public void equipWeapon(Weapon weapon) throws ArcherInvalidWeaponException {
+            if (level < weapon.getRequiredLevel()) {
+                throw new ArcherInvalidWeaponException("Hero level is too low to equip this weapon");
+            }
 
-}
+            if (!WeaponType.BOW.equals(weapon.getWeaponType())) {
+                throw new ArcherInvalidWeaponException("Archers can only use bows");
+            }
+
+            equipment.equipItem(weapon);
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public int getLevel() {
+            return level;
+        }
+
+        public HeroAttribute getLevelAttributes() {
+            return levelAttributes;
+        }
+
+    }
--- src/test/java/no/noroff/accelerate/hero/heroclasses/ArcherTest.java
+++ src/test/java/no/noroff/accelerate/hero/heroclasses/ArcherTest.java
@@ -1,8 +1,6 @@
 package no.noroff.accelerate.hero.heroclasses;
 
-import no.noroff.accelerate.exceptions.weapons.ArcherInvalidWeaponException;
 import no.noroff.accelerate.exceptions.weapons.InvalidWeaponException;
-import no.noroff.accelerate.hero.Hero;
 import no.noroff.accelerate.hero.HeroAttribute;
 import no.noroff.accelerate.items.weapons.Weapon;
 import no.noroff.accelerate.items.weapons.WeaponType;
@@ -13,72 +11,148 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class ArcherTest {
     @Test
-    public void testNewHeroArcherCreation() {
+    public void testIfArcherIsCreatedWithCorrectName() {
         // Arrange
-        Archer archer = new Archer("Sniperman");
+        final String expectedName = "Sniperman";
 
-        //Act and assert
-        assertEquals("Sniperman", archer.getName());
-        assertEquals(1, archer.getLevel());
-        assertEquals(1, archer.getLevelAttributes().getStrength());
-        assertEquals(7, archer.getLevelAttributes().getDexterity());
-        assertEquals(1, archer.getLevelAttributes().getIntelligence());
+        // Act
+        Archer archer = new Archer(expectedName);
+        final String actualName = archer.getName();
 
+        // Assert
+        assertEquals(expectedName, actualName);
+    }
+
+    @Test
+    public void testIfArcherIsCreatedWithCorrectLevelWhichShouldBeOne() {
+        //Arrange
+        final String archerName = "Sniperman";
+        final int expectedLevel = 1;
+
+        //Act
+        Archer archer = new Archer(archerName);
+        final int actualLevel = archer.getLevel();
+
+        //Assert
+        assertEquals(expectedLevel, actualLevel);
     }
 
     @Test
     public void testIfArcherLevelIncreases() {
         //Arrange
-        Archer archer = new Archer("Sniperman");
-        assertEquals(1, archer.getLevel());
+        final int expectedLevel = 2;
+        final String archerName = "Sniperman";
+        Archer archer = new Archer(archerName);
 
         // Act
         archer.levelUp();
+        final int actualLevel = archer.getLevel();
 
         //Assert
-        assertEquals(2, archer.getLevel());
+        assertEquals(expectedLevel, actualLevel);
 
     }
 
     @Test
-    public void testCheckIfArcherAttributesIncreasesByTheRightAmountWhenLevellingUp() {
+    public void testIfArchersDefaultStrengthAttributeIsCorrectWhenCreated(){
         //Arrange
-        Archer archer = new Archer ("Sniperman");
+        final int expectedAttribute = 1;
+        final String archerName = "Sniperman";
 
         //Act
-        HeroAttribute expectedAttributeLevelUp = new HeroAttribute(1, 5, 1);
-        HeroAttribute actualAttributeLevelUp = archer.getAttributeLevelUp();
+        Archer archer = new Archer(archerName);
+        final int actualStrengthAttribute = archer.getLevelAttributes().getStrength();
+
 
         //Assert
-        assertEquals(expectedAttributeLevelUp.getStrength(), actualAttributeLevelUp.getStrength());
-        assertEquals(expectedAttributeLevelUp.getDexterity(), actualAttributeLevelUp.getDexterity());
-        assertEquals(expectedAttributeLevelUp.getIntelligence(), actualAttributeLevelUp.getIntelligence());
-
+        assertEquals(expectedAttribute, actualStrengthAttribute);
     }
 
     @Test
-    public void testChechIfArcherAttributesLevelUpCorrectlyWhenTheArcherLevelsUp() {
+    public void testIfArchersDefaultDexterityAttributeIsCorrectWhenCreated(){
         //Arrange
-        Archer archer = new Archer("Sniperman");
+        final int expectedAttribute = 7;
+        final String archerName = "Sniperman";
+
+        //Act
+        Archer archer = new Archer(archerName);
+        final int actualDexterityAttribute = archer.getLevelAttributes().getDexterity();
+
+        //Assert
+        assertEquals(expectedAttribute, actualDexterityAttribute);
+    }
+
+    @Test
+    public void testIfArchersDefaultIntelligenceAttributeIsCorrectWhenCreated(){
+        //Arrange
+        final int expectedAttribute = 1;
+        final String archerName = "Sniperman";
+
+        //Act
+        Archer archer = new Archer(archerName);
+        final int actualIntelligenceAttribute = archer.getLevelAttributes().getIntelligence();
+
+        //Assert
+        assertEquals(expectedAttribute, actualIntelligenceAttribute);
+    }
+
+    @Test
+    public void testCheckIfArcherStrengthAttributeIncreasesByTheRightAmountWhenLevellingUp() {
+        //Arrange
+        final int expectedAttribute = 2;
+        final String archerName = "Sniperman";
+        Archer archer = new Archer(archerName);
 
         //Act
         archer.levelUp();
+        final int actualStrengthOnLvlUp = archer.getLevelAttributes().getStrength();
 
         //Assert
-        assertEquals(2, archer.getLevel());
-        assertEquals(2, archer.getLevelAttributes().getStrength());
-        assertEquals(12, archer.getLevelAttributes().getDexterity());
-        assertEquals(2, archer.getLevelAttributes().getIntelligence());
+        assertEquals(expectedAttribute, actualStrengthOnLvlUp);
+    }
 
+    @Test
+    public void testCheckIfArcherDexterityAttributeIncreasesByTheRightAmountWhenLevellingUp() {
+        //Arrange
+        final int expectedAttribute = 12;
+        final String archerName = "Sniperman";
+        Archer archer = new Archer(archerName);
+
+        //Act
+        archer.levelUp();
+        final int actualDexterityOnLvlUp = archer.getLevelAttributes().getDexterity();
+
+        //Assert
+        assertEquals(expectedAttribute, actualDexterityOnLvlUp);
+    }
+
+    @Test
+    public void testCheckIfArcherIntelligenceAttributeIncreasesByTheRightAmountWhenLevellingUp() {
+        //Arrange
+        final int expectedAttribute = 2;
+        final String archerName = "Sniperman";
+        Archer archer = new Archer(archerName);;
+
+        //Act
+        archer.levelUp();
+        final int actualIntelligenceOnLvlUp = archer.getLevelAttributes().getIntelligence();
+
+        //Assert
+        assertEquals(expectedAttribute, actualIntelligenceOnLvlUp);
     }
 
     @Test
     public void testCheckIfArcherCanEquipWeaponWithValidWeaponTypeAndLevel() {
         //Arrange
-        Archer archer = new Archer("Sniperman");
-        Weapon weapon = new Weapon("Primitive bow", 1, WeaponType.BOW, 10);
+        final String archerName = "Sniperman";
+        final String weaponName = "Primitve bow";
+        final int requiredLevel = 1;
+        final int weaponDamage = 10;
 
         //Act
+        Archer archer = new Archer(archerName);
+        Weapon weapon = new Weapon(weaponName, requiredLevel, WeaponType.BOW, weaponDamage);
+
         try {
             archer.equipWeapon(weapon);
         } catch (InvalidWeaponException e) {
@@ -86,57 +160,113 @@ class ArcherTest {
         }
 
         //Assert
-        assertEquals(weapon, archer.getEquippedWeapon());
+        Weapon expectedEquippedWeapon = weapon;
+        Weapon actualEquippedWeapon = archer.getEquippedWeapon();
+
+        assertEquals(expectedEquippedWeapon, actualEquippedWeapon);
     }
 
     @Test
     public void testCheckIfWhenArcherIsEquippingAWeaponWithValidTypeButTooHighLevelThrowsCorrectException() {
         //Arrange
-        Archer archer = new Archer("Sniperman");
-        Weapon weapon = new Weapon("Barret .50 cal", 30, WeaponType.BOW, 500);
+        final String archerName = "Sniperman";
+        final String weaponName = "Barret .50 cal";
+        final int requiredLevel = 30;
+        final int weaponDamage = 500;
 
-        //Act & Assert
-        InvalidWeaponException exception = assertThrows(InvalidWeaponException.class, () ->  {
+        //Act
+        Archer archer = new Archer(archerName);
+        Weapon weapon = new Weapon(weaponName, requiredLevel, WeaponType.BOW, weaponDamage);
+
+        InvalidWeaponException expectedException = new InvalidWeaponException("Hero level is too low to equip this weapon");
+        InvalidWeaponException actualException = assertThrows(InvalidWeaponException.class, () -> {
             archer.equipWeapon(weapon);
         });
 
-        assertEquals("Hero level is too low to equip this weapon", exception.getMessage());
+        ////Assert
+        assertEquals(expectedException.getMessage(), actualException.getMessage());
     }
 
     @Test
     public void testCheckIfWhenArcherIsEquippingAWeaponWithInvalidTypeButValidLevelThrowsCorrectException() {
         //Arrange
-        Archer archer = new Archer("Sniperman");
-        Weapon weapon = new Weapon("Hidden Blade", 1, WeaponType.DAGGER, 1000);
+        final String archerName = "Sniperman";
+        final String weaponName = "Hidden Blade";
+        final int requiredLevel = 1;
+        final int weaponDamage = 1000;
 
-        //Act & Assert
-        InvalidWeaponException exception = assertThrows(InvalidWeaponException.class, () ->  {
+        //Act
+
+        Archer archer = new Archer(archerName);
+        Weapon weapon = new Weapon(weaponName, requiredLevel, WeaponType.DAGGER, weaponDamage);
+
+        InvalidWeaponException expectedException = new InvalidWeaponException("Archers can only use bows");
+        InvalidWeaponException actualException = assertThrows(InvalidWeaponException.class, () -> {
             archer.equipWeapon(weapon);
         });
 
-        assertEquals("Archers can only use bows", exception.getMessage());
+        //Assert
+        assertEquals(expectedException.getMessage(), actualException.getMessage());
     }
 
     @Test
     public void testCalculateArcherDamageWithNoWeaponEquipped() {
-        Archer archer = new Archer("Sniperman");
+        //Arrange
+        final String archerName = "Sniperman";
+        final int expectedDamage = 1;
 
-        double calculatedDamage = archer.calcDamage();
+        //Act
+        Archer archer = new Archer(archerName);
+        final double actualDamage = archer.calcDamage();
 
-        assertEquals(1, calculatedDamage);
+        //Assert
+        assertEquals(expectedDamage, actualDamage);
     }
 
     @Test
     public void testCalculateIfArcherDamageIsCalculatedProperlyWhenAWeaponIsEquipped() {
-        Archer archer = new Archer("Sniperman");
-        Weapon weapon = new Weapon("Enchanted bow", 1, WeaponType.BOW, 35);
+        //Arrange
+        final String archerName = "Sniperman";
+        final String weaponName = "Enchanted bow";
+        final int requiredLevel = 1;
+        final int weaponDamage = 35;
+
+        //Act
+        Archer archer = new Archer(archerName);
+        Weapon weapon = new Weapon(weaponName, requiredLevel, WeaponType.BOW, weaponDamage);
 
         archer.equipWeapon(weapon);
 
-        double calculatedDamage = archer.calcDamage();
+        final double actualDamage = archer.calcDamage();
+        final double expectedDamage = weaponDamage * (1 + (double) archer.getLevelAttributes().getDexterity() / 100);
 
-        assertEquals(37.45, calculatedDamage);
+
+        //Assert
+        assertEquals(expectedDamage, actualDamage);
     }
 
+    @Test
+    public void testCalculateIfArcherDamageIsCalculatedProperlyWhenAWeaponIsEquippedAndADifferentOneIsEquippedAgain() {
+        //Arrange
+        final String archerName = "Sniperman";
+        final String weaponName = "Enchanted bow";
+        final String weapon2Name = "Master bow";
+        final int requiredLevel = 1;
+        final int weaponDamage = 35;
+        final int weapon2Damage = 90;
+
+        //Act
+        Archer archer = new Archer(archerName);
+        Weapon weapon = new Weapon(weaponName, requiredLevel, WeaponType.BOW, weaponDamage);
+        Weapon weapon2 = new Weapon(weapon2Name, requiredLevel, WeaponType.BOW, weapon2Damage);
+
+        archer.equipWeapon(weapon);
+        archer.equipWeapon(weapon2);
+
+        final double calculatedDamage = archer.calcDamage();
+        final double expectedDamage = weapon2Damage * (1 + (double) archer.getLevelAttributes().getDexterity() / 100);
+
+        assertEquals(expectedDamage, calculatedDamage);
+    }
 
 }
